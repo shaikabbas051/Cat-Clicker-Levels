6baba1bba3ab7075433eee091923e1a7de45745b
shaik abbas <shaikabbas051@gmail.com>
2017-10-03 17:46:01 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,38 @@
+function viewModel(){
+		this.name = "Anna";
+		this.score = ko.observable(0);
+		this.imgsrc = "img/434164568_fea0ad4013_z.jpg";
+		this.incrementcounter = function(){
+			this.score(this.score()+1);
+		};
+		
+		/*this.generation = ko.computed(function(){
+			return this.score() >= 5 ? "infant" : "child";
+			return this.score() >= 10 ? "teen" : "child";
+		}, this);*/
+
+		this.level = ko.computed(function(){
+			var clicks = this.score();
+			if(clicks >= 6 && clicks <= 10){
+				return "Infant";
+			}
+			if(clicks >= 11 && clicks <= 15){
+				return "Teen";
+			}
+			if(clicks >= 15 && clicks <= 20){
+				return "Adult";
+			}
+			if(clicks >= 20 && clicks <= 25){
+				return "Midlife";
+			}
+			if(clicks >= 25){
+				return "Old";
+			}
+			else{
+				return "Newborn"
+			}
+		}, this);
+}
+
+ko.applyBindings(new viewModel());
+
