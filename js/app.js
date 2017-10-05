6c8ec76ea1787f904aa16a2a11a92393fd8d3f19
shaik abbas <shaikabbas051@gmail.com>
2017-10-05 10:51:35 +0530

    added new images

--- js/app.js
+++ js/app.js
@@ -1,9 +1,40 @@
+var initialcats = [{
+				name: "Anne",
+				imgsrc: "images/cat1.jpg",
+				score: 0,
+				nameslist: "Anda"
+				},
+				{
+				name: "Beca",
+				imgsrc: "images/cat2.jpeg",
+				score: 0,
+				nameslist: "Balu"
+				},
+				{
+				name: "Cary",
+				imgsrc: "images/cat3.jpg",
+				score: 0,
+				nameslist: "Cutie"
+				},
+				{
+				name: "Dani",
+				imgsrc: "images/cat4.jpg",
+				score: 0,
+				nameslist: "Danger"
+				},
+				{
+				name: "Erie",
+				imgsrc: "images/cat5.jpg",
+				score: 0,
+				nameslist: "English"
+				}];
+
+
 var cats = function(data){
 
 		this.name = ko.observable(data.name);
 		this.score = ko.observable(data.score);
 		this.imgsrc = ko.observable(data.imgsrc);
-		this.nickname = ko.observable(data.nickname);
 		this.nameslist = ko.observableArray(data.nameslist);
 
 		this.level = ko.computed(function(){
@@ -24,33 +55,31 @@ var cats = function(data){
 				return "Old";
 			}
 			else{
-				return "Newborn"
+				return "Newborn";
 			}
 		}, this);
-}
+};
 
 var viewModel = function(){
 	var self = this;
-	this.currentcat = ko.observable(new cats(
-		{
-			name: "Anna",
-			score: 0,
-			nickname: "NickNames",
-			imgsrc: "img/434164568_fea0ad4013_z.jpg",
-			nameslist: ["Cutie","Puppy","Raju"]
-		}));
 
-		this.incrementcounter = function(){
+	this.catlist = ko.observableArray([]);
+
+	initialcats.forEach(function(catitem){
+		self.catlist.push(new cats(catitem));
+	});
+
+	this.currentcat = ko.observable(this.cats()[0]);
+
+	this.incrementcounter = function(){
 		//this.score(this.score()+1);
 		self.currentcat().score(self.currentcat().score()+1);
-		};
-}
+	};
+
+	this.setcat = function(clickedcat){
+		self.currentcat(clickedcat);
+	};
+
+};
 	
-
-
-		
-
-
-
 ko.applyBindings(new viewModel());
-
